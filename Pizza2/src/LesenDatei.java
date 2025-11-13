5d3a7d430c2bff7b155a58c81ec96534957c9908
willmsmarian <willmsmarian@104-03.bsz-wuerzburg.de>
2025-11-13 09:19:04 +0100

    commit

--- Pizza2/src/LesenDatei.java
+++ Pizza2/src/LesenDatei.java
@@ -2,16 +2,29 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class LesenDatei {
-    public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(new FileReader("H:\\git\\TestJava2025-26\\Pizza2\\src\\Speisekarte.txt"
-        		+ ""))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-            }
-        } catch (IOException e) {
-            System.out.println("Error reading file: " + e.getMessage());
-        }
-    }
+public class LesenDatei
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			String regex = ";";
+			String line;
+			try (BufferedReader br = new BufferedReader(new FileReader("H:\\git\\TestJava2025-26\\Pizza2\\src\\Speisekarte.txt" + "")))
+			{
+				while ((line = br.readLine()) != null)
+				{
+				    String[] myArray = line.split(regex);
+				    for (String s : myArray)
+				    {
+					    System.out.println(s);
+				    }
+				}
+			}
+		}
+		catch (IOException e)
+		{
+			System.out.println("Error reading file: " + e.getMessage());
+		}
+	}
 }
